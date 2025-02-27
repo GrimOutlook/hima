6c9d27db1f8271f12544044fefff82a91568e0a2
Dominic Grimaldi <dominic.j.grimaldi2.ctr@army.mil>
2025-02-27 12:02:38 -0600

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Dialog } from '@headlessui/react';
+// import { Dialog } from '@headlessui/react';
 
 import EventsList from "./components/events_list";
 import PoolList from "./components/pools_list";
@@ -7,7 +7,7 @@ import TotalHoursCard from "./components/total_hours_card";
 
 export default function Home() {
   return (
-    <div className="flex flex-row bg-gradient-to-tr to-zinc-900 from-zinc-700">
+    <div className="flex flex-row text-gray-700 bg-gradient-to-tl to-zinc-900 from-zinc-700">
         {/* <!-- Left side --> */}
         <div className="flex flex-col min-w-64 w-1/3 ml-2 my-2">
             {/* <!-- Top box --> */}
