667b985b58aab3beb7c177ef260f7445bb146e97
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-03-04 10:36:25 -0600

    Fixed some formatting of the events listing and updated the README

--- README.md
+++ README.md
@@ -10,12 +10,21 @@
 - [x] Calculate amout of PPL in a pool at a certain date.
 - [x] Calculate total amount of PPL available.
 - [x] Create a form to add events.
-- [ ] Do the above but add events into the equation.
+- [x] Do the above but add events into the equation.
+- [ ] Show details on hover.
+- [ ] Allow deletion of events.
+- [ ] Allow deletion of pools.
 - [ ] Validate inputs before creating pool (Can currently add pool without a name or a date).
 - [ ] Validate inputs before creating event.
 - [ ] Create settings menu.
 - [ ] Show error when trying to create an event with no pool created.
 - [ ] Allow user to check PPL amount at future or past dates.
+- [ ] Allow export of data to JSON.
+- [ ] Allow import of data from JSON.
+- [ ] Allow selecting colors for pools.
+- [ ] Allow user to filter pools by selection.
+  - Click on pool and only that pool's events show in the events list.
+- [ ] Allow user to show total in days.
 
 ### Getting Started
 
--- src/app/components/event_listing.tsx
+++ src/app/components/event_listing.tsx
@@ -13,17 +13,16 @@ type EventListingProps = {
 const EventListing: React.FC<EventListingProps> = ({className, date, title, hours, pool}) => {
     return (
         <div className={`${className} flex flex-row bg-red-400 m-2 rounded-lg h-18 transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-102 hover:drop-shadow-lg cursor-pointer`}>
-            
             {/* <!-- Date --> */}
-            <div className="flex flex-col justify-center items-center h-full w-24 p-2">
-                <div className="text-4xl h-2/3 w-full text-center overflow-hidden text-ellipsis">{date.format("M/D")}</div>
+            <div className="h-full self-justify-center self-center w-32 p-2">
+                <div className="text-3xl h-2/3 w-full text-center overflow-hidden">{date.format("MM/DD")}</div>
             </div>
             {/* <!-- Hours --> */}
             <div className="flex flex-col justify-center items-center h-full w-24 p-2">
                 <div className="text-4xl h-2/3 w-full text-center overflow-hidden text-ellipsis">{hours}</div>
                 <div className="text-sm h-1/3 w-full text-center">hrs</div>
             </div>
-            <div className="flex flex-col w-full m-2">
+            <div className="flex flex-col w-full m-2 self-center">
                 {/* <!-- Title of event --> */}
                 <div className="text-xl h-full w-full overflow-hidden text-ellipsis font-normal">{title}</div>
                 {/* <!-- Pool name --> */}
