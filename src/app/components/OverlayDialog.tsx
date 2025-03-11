c89c73fe6012fb1916171af2a95f7943c2814af7
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-03-11 12:35:06 -0500

    OverlayDialog now adheres to recommended eslint rules

--- src/app/components/OverlayDialog.tsx
+++ src/app/components/OverlayDialog.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Dialog, Transition, TransitionChild } from "@headlessui/react";
-import { Fragment, JSX } from "react";
+import React, { Fragment, JSX } from "react";
 
 type OverlayDialogProps = {
   children?: string | JSX.Element | JSX.Element[];
@@ -13,25 +13,23 @@ export const OverlayDialog: React.FC<OverlayDialogProps> = ({
   children,
   show,
   onClose,
-}) => {
-  return (
-    <Transition appear show={show} as={Fragment}>
-      <Dialog open={show} onClose={onClose} as="div" className="relative z-10">
-        <TransitionChild
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black/50" onClick={onClose} />
-        </TransitionChild>
-        <div className="fixed inset-0 self-center justify-self-center">
-          {children}
-        </div>
-      </Dialog>
-    </Transition>
-  );
-};
+}) => (
+  <Transition appear show={show} as={Fragment}>
+    <Dialog open={show} onClose={onClose} as="div" className="relative z-10">
+      <TransitionChild
+        as={Fragment}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 bg-black/50" onClick={onClose} />
+      </TransitionChild>
+      <div className="fixed inset-0 self-center justify-self-center">
+        {children}
+      </div>
+    </Dialog>
+  </Transition>
+);
