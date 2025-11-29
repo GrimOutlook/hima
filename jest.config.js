c86c2028eac9eed013ec8fe9aa11ff2c4abc88d6
Dominic Grimaldi <self@grimoutlook.dev>
2025-11-29 11:37:58 -0600

    chore: Initialize expo refactor

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
-  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
